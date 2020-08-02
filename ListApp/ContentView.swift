1f284327273c5eb25e7e339476300867f8858ea3
iwasakieisuke <iwasaki@centsys.jp>
2020-08-02 22:47:55 +0900

    Initial Commit

---
+++ ListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListApp
+//
+//  Created by 岩崎瑛佑 on 2020/08/02.
+//  Copyright © 2020 岩崎瑛佑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
